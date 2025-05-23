bb513cb1e2c3076c32ba4a5777a2a5499c858f48
willmeston1 <will.meston@bellevuecollege.edu>
2025-05-22 23:49:43 -0700

    Update index.js

--- a3/index.js
+++ a3/index.js
@@ -11,7 +11,23 @@ downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
-function upLeft (pHeight, pColorEven, pColorOdd, pSymbol){}
+function upLeft (pHeight, pColorEven, pColorOdd, pSymbol){
+  var rLine ="";
+  for (i=0;i<pHeight;i++){
+  rLine +="<p>";
+  for(j=0;j<=i;j++){
+
+  if (j%2)
+  rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+  else
+  rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+  }
+    
+  rLine +="</p>";
+  }
+  
+  document.getElementById("upLeft").innerHTML = rLine;
+  }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
@@ -37,7 +53,22 @@ rLine +="</p>";
 document.getElementById("upRight").innerHTML = rLine;
 }
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){}
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+for(j=0;j<i;j++){
+  
+if (j%2)
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
+else
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+
+}
+rLine +="</p>";
+}
+document.getElementById("downLeft").innerHTML = rLine;
+}
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
