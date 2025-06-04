551408b0f1145252d12347f6d8a3a4cc94d1a7df
willmeston1 <will.meston@bellevuecollege.edu>
2025-06-04 01:21:38 -0700

    Update validator.js

--- a4/validator.js
+++ a4/validator.js
@@ -23,16 +23,39 @@ function firstName(){
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
         console.log("First name invalid — length")
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            errorMessages += "<p>Invalid caracter in first name (accepts only A-Z, a-z, and ,.'-)</p>";
             console.log("First name invalid — bad characters")
         } else {
                 validFirstname = true;
-                console.log("First name valid")
-        };
-
+                console.log("First name valid");
+                  };
     //4) Send error message to HTML
     document.getElementById("fname").innerHTML = errorMessages;
 
     //5) return status of each field
     return (validFirstname);
 };
+  
+  LastName.addEventListener('blur', lastName, false);
+  function lastName(){
+  //1) Create variable
+var validLastname=false;
+//2) read value from HTML
+var lastname = document.getElementById("lastname").value;
+
+//3) Do validation
+    if (lastname==="null" || lastname==="" || lastname.length > 20 ) {
+      errorMessages += "<p>The last name is required and cannot be greater than 20 characters</p>";
+      console.log("Last name invalid - length")
+    } else if (lastname.match("^[a-zA-Z ,.'-]+$")===null) {
+      errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+      console.log("First name invalid — bad characters")
+    } else {
+      validLastname = true;
+      console.log("Last name valid")
+    }
+//4) Send error message to HTML
+       document.getElementById("lname").innerHTML = errorMessages;
+//5) return status of each field
+return (validFirstname && validLastname);
+  };
