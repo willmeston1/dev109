0f9837064362fef23de64bdf979c0c50d5e2b80b
willmeston1 <will.meston@bellevuecollege.edu>
2025-06-04 16:22:09 -0700

    Update validator.js

--- a4/validator.js
+++ a4/validator.js
@@ -11,14 +11,11 @@ function isValid() {
 
 FirstName.addEventListener('blur', firstName, false);
 function firstName(){
-    //1) Create variable
     var validFirstname=false;
 
-    //2) read value from HTML
     var firstname = document.getElementById("FirstName").value;
     var errorMessages = "";
 
-    //3) Do validation
     if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
         console.log("First name invalid — length")
@@ -28,22 +25,17 @@ function firstName(){
         } else {
                 validFirstname = true;
                 console.log("First name valid");
-                  };
-    //4) Send error message to HTML
+                  }
     document.getElementById("fname").innerHTML = errorMessages;
 
-    //5) return status of each field
     return (validFirstname);
 }
-  
+  //To do: Remove [object HTMLCollection] text
   LastName.addEventListener('blur', lastName, false);
   function lastName(){
-  //1) Create variable
 var validLastname=false;
-//2) read value from HTML
 var lastname = document.getElementById("LastName").value;
 
-//3) Do validation
     if (lastname==="null" || lastname==="" || lastname.length > 20 ) {
       errorMessages += "<p>The last name is required and cannot be greater than 20 characters</p>";
       console.log("Last name invalid - length")
@@ -54,23 +46,35 @@ var lastname = document.getElementById("LastName").value;
       validLastname = true;
       console.log("Last name valid")
     }
-//4) Send error message to HTML
+
        document.getElementById("lname").innerHTML = errorMessages;
-//5) return status of each field
+
 return (validLastname);
   }
 
-//To do: Username error also shows up under Last Name text box
-  Username.addEventListener('blur', username, false);
-  function username(){
+Email.addEventListener('blur', email, false);
+ function email() {
+   var userEmail = document.getElementById("email").value;
+var atpos = userEmail.indexOf("@");
+var dotpos = userEmail.lastIndexOf(".");
+if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+    errorMessages += "<p>Invalid email</p>";
+} else {
+    return true;
+ }
+ }
+
+//To do: Remove [object HTMLCollection] text
+  Username.addEventListener('blur', userName, false);
+  function userName(){
     var validUsername = false;
-    var userName = document.getElementById("Username").value;
-    if (userName==="null" || userName==="" || userName.length > 12 ) {
-      errorMessages += "<p>Username is required and cannot be greater than 12 characters</p>";
+    var username = document.getElementById("Username").value;
+    if (username==="null" || username==="" || username.length > 12 ) {
+      errorMessages += "<p>Username is required; cannot be greater than 12 characters</p>";
 } else {
   validUsername = true;
   console.log ("Username valid")
 }
     document.getElementById("username").innerHTML = errorMessages;
-    return (validUsername);
+    return true;
   }
