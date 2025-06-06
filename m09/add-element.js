828d32fa0065cd577e43e1f57e733211b7eb326b
willmeston1 <will.meston@bellevuecollege.edu>
2025-06-05 17:25:09 -0700

    Create add-element.js

---
+++ m09/add-element.js
@@ -0,0 +1,13 @@
+function addListItem() {
+var newEl = document.createElement('li');
+var newText = document.createTextNode('quinoa');
+
+// Attach the new text node to the new element.
+newEl.appendChild(newText);
+
+// Find the position where the new element should be added.
+var position = document.getElementsByTagName('ul')[0];
+
+// Insert the new element into its position.
+position.appendChild(newEl);
+}
