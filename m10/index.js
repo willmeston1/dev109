d15662b6075551ab4233daf286acc907580f4c5a
willmeston1 <will.meston@bellevuecollege.edu>
2025-06-15 02:28:35 -0700

    Update index.js

--- m10/index.js
+++ m10/index.js
@@ -13,33 +13,30 @@ event.stopPropagation();
   });
 
 
-  addEventListener("click", function(event) {
+ document.addEventListener("click", function(event) {
     var dot = document.createElement("div");
     dot.className = "dot";
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
-    
-     updateDot(dot);
   });
 
-  function updateDot() {
-    let dot = document.querySelector(".dot");
+function updateDot(dot) {
     let newColor = document.getElementById("color").value;
     let newSize = document.getElementById("size").value + "px";
-    let newBorder =document.getElementById("size").value + "px";
-    
-    dot.classList.add("dot");
+
     dot.style.height = newSize;
     dot.style.width = newSize;
     dot.style.backgroundColor = newColor;
+}
 
+// Event listeners to detect user input changes
+document.getElementById("color").addEventListener("change", function() {
+    let lastDot = document.querySelector(".dot:last-of-type");
+    if (lastDot) updateDot(lastDot);
+});
 
-    dot.style.backgroundColor = newColor;
-    dot.style.height = newSize;
-    dot.style.width = newSize;
-  }
-
-  // Event listeners to detect user input changes
-  document.getElementById("color").addEventListener("change", updateDot);
-  document.getElementById("size").addEventListener("input", updateDot);
+document.getElementById("size").addEventListener("input", function() {
+    let lastDot = document.querySelector(".dot:last-of-type");
+    if (lastDot) updateDot(lastDot);
+});
