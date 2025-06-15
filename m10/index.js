973691a6e59763fe8e6bef26bb927b5e281194d6
willmeston1 <will.meston@bellevuecollege.edu>
2025-06-15 02:21:36 -0700

    Update index.js

--- m10/index.js
+++ m10/index.js
@@ -1,4 +1,3 @@
-
 var  mybutton = document.querySelector("button");
 mybutton.addEventListener("click", function(event) {
 
@@ -12,18 +11,29 @@ for (index = element.length - 1; index >= 0; index--) {
 
 event.stopPropagation();
   });
+
+
   addEventListener("click", function(event) {
     var dot = document.createElement("div");
     dot.className = "dot";
     dot.style.left = (event.pageX - 4) + "px";
     dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
+    
+     updateDot(dot);
   });
 
   function updateDot() {
     let dot = document.querySelector(".dot");
     let newColor = document.getElementById("color").value;
     let newSize = document.getElementById("size").value + "px";
+    let newBorder =document.getElementById("size").value + "px";
+    
+    dot.classList.add("dot");
+    dot.style.height = newSize;
+    dot.style.width = newSize;
+    dot.style.backgroundColor = newColor;
+
 
     dot.style.backgroundColor = newColor;
     dot.style.height = newSize;
